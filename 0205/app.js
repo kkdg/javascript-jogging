6ccce428ba1c673a1ab96488b782b4d1f4b886c6
Kwag De Gi <code@skyaperture.com>
2015-02-05 07:25:31 +0900

    for/in

--- 0205/app.js
+++ 0205/app.js
@@ -6,3 +6,11 @@ var v=true
 var v={p1:2,p2:"2"}
 var v=[1,2,3,4]
 
+
+var myObjet = { p1: 'a', p2: 'b'};
+var result='\n';
+for (var prop in myObjet) {
+	result += '속성명: ' + prop + ', 값: ' + myObjet[prop] + '\n';
+}
+
+
