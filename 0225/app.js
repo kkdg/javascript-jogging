53fd1df218dd3282fb9e08ae48ecf16a215caca4
Kwag De Gi <code@skyaperture.com>
2015-02-25 10:29:12 +0900

    All numbers are float type

--- 0225/app.js
+++ 0225/app.js
@@ -7,3 +7,11 @@ function square(x) {
 	return x*x
 }
 console.log(square);
+
+var n1 = 255;
+var n2 = 255.0;
+var n3 = 0377;
+var n4 = 0xff;
+
+console.log(typeof n1, typeof n2, typeof n3, typeof n4);
+
